9749858aaa4349be24b32aa3c2c13cbc75add673
Stephen Prochnow <stephen.prochnow@gmail.com>
2015-06-12 19:26:16 +0200

    Implemented sample forecast data

--- app/src/main/java/com/example/prochnow/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/prochnow/sunshine/app/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package com.example.prochnow.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -12,12 +15,17 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    String[] forecastDataArray = {"Today - Sunny - 88 / 63", "Tomorrow - Foggy- 70 / 46", "Weds - Cloudy - 72 / 81", "Thus - Rainy- 64 / 51", "Fri - Foggy - 70 / 46", "Sat - Sunny - 76 / 68"};
+
+    ArrayList<String> forecastData = new ArrayList<String>(Arrays.asList(forecastDataArray));
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }

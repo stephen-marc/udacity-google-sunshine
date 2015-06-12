e041d3d560a6f44f940f5090d3b6cb7f744a23e2
Stephen Prochnow <stephen.prochnow@gmail.com>
2015-06-12 19:38:19 +0200

    Added adapter with sample data into listview in fragment

--- app/src/main/java/com/example/prochnow/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/prochnow/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,7 +19,7 @@ public class MainActivityFragment extends Fragment {
 
     String[] forecastDataArray = {"Today - Sunny - 88 / 63", "Tomorrow - Foggy- 70 / 46", "Weds - Cloudy - 72 / 81", "Thus - Rainy- 64 / 51", "Fri - Foggy - 70 / 46", "Sat - Sunny - 76 / 68"};
 
-    ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastDataArray));
+    ArrayList<String> weekForecast = new ArrayList<>(Arrays.asList(forecastDataArray));
 
     public MainActivityFragment() {
     }
@@ -26,7 +27,12 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 }
